c220a2a6f246528d6fa437e9593966e6908bf1ae
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2014-05-24 13:48:19 +0200

    Added comments to avr_crosscompile.cmake

--- cmake/avr_crosscompile.cmake
+++ cmake/avr_crosscompile.cmake
@@ -10,7 +10,11 @@ set(CMAKE_SYSTEM_NAME Generic)
 # what the cxx compiler and binutils are called.
 set(CMAKE_C_COMPILER avr-gcc)
 
-set(PROGRAMMER avrispmkII)
+# Setup the programmer that is used to write to the MCU
+# run `avrdude -c ?` for a list of valid ID's
+# run `avrdude -p ?` for a list of valid MCU's
+find_program(PROGRAMMER avrdude)
+set(PROGRAMMER_ID avrispmkII)
 set(PROGRAMMER_MCU c128)
 
 # Make sure we have a working copy of the correct objcopy
@@ -19,9 +23,8 @@ if(NOT CMAKE_OBJCOPY)
     set(CMAKE_OBJCOPY ${AVROBJCOPY_PROGRAM})
 endif(NOT CMAKE_OBJCOPY)
 
-find_program(AVRDUDE avrdude)
-
-# Generates an intel hex file from a compile binary elf file
+# Sets up a custom command and target that Generates an intel hex file from a
+# compiled binary elf file.
 function(avr_create_hex elf_file)
 	add_custom_command(
 		OUTPUT ${elf_file}.hex
@@ -38,15 +41,17 @@ function(avr_create_hex elf_file)
 		)
 endfunction()
 
+# Sets up a target that calls the programmer to flash the MCU with the given
+# binary. It automaticly converts the elf_file to the correct hex format
 function(avr_make_flashable elf_file)
 	avr_create_hex(${elf_file})
 
 	add_custom_target(${elf_file}_writeflash
-		COMMAND ${AVRDUDE}
-		ARGS 	-c ${PROGRAMMER}
-				-p ${PROGRAMMER_MCU}
-				-P usb -B 8
-				-U flash:w:${elf_file}.hex
+		COMMAND ${PROGRAMMER}
+					-c ${PROGRAMMER_ID}
+					-p ${PROGRAMMER_MCU}
+					-P usb -B 8
+					-U flash:w:${elf_file}.hex
 		DEPENDS ${elf_file}_hex
 		VERBATIM
 		)
