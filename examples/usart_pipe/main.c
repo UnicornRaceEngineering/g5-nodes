45b3b00f6f903130096f703ee4bcefad19e7fdad
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2014-06-10 22:16:59 +0200

    We can now request ECU for data

--- examples/usart_pipe/main.c
+++ examples/usart_pipe/main.c
@@ -24,19 +24,41 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdlib.h> // size_t
 #include <avr/interrupt.h> // sei()
 #include <usart.h>
+#include <util/delay.h>
+
+#define XBEE_BAUD 	(115200)
+#define ECU_BAUD	(19200)
+
+// rapidly firring this causes the ECU to crash. Therefor we need to call this
+// periodically.
+static void request_ecu(void) {
+	const uint8_t start_seq[10] = {0x12,0x34,0x56,0x78,0x17,0x08,0,0,0,0};
+	for (int i = 0; i < 10; ++i) {
+		usart0_putc(start_seq[i]);
+	}
+}
 
 int main(void) {
-	usart0_init(115200); // xbee
-	usart1_init(19200);	 // ECU
+	usart1_init(XBEE_BAUD); // xbee
+	usart0_init(ECU_BAUD);	// ECU
 
 	sei();										//Enable interrupt
 
+	usart1_puts("\nStarting:\n");
+
 	while(1){
 		// Main work loop
-		//usart1_puts("World\n");
-		//usart0_puts("Hello\n");
-		//usart0_printf("0x%x\n", usart1_getc());
-		usart0_putc(usart1_getc());
+
+		usart1_puts("requesting data\n");
+		request_ecu();
+		_delay_ms(10);
+
+		for (int i = 0; i < 115; ++i) {
+			//usart1_printf("[%d: 0x%x], ",i, usart0_getc());
+			usart1_printf("[%d: %d], ", i, usart0_getc());
+		}
+		usart1_putc('\n');
+
 	}
 
     return 0;
