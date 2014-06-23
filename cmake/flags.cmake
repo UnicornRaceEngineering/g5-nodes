1852fa83df89f5ba541741e697aaad0051c9d6f0
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2014-06-23 16:33:31 +0200

    Correctly setting compile time defines in cmake

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,11 +1,9 @@
-set(F_CPU "-DF_CPU=11059200")
-set(CAN_BAUDRATE "-DCAN_BAUDRATE=250")
+set(F_CPU 11059200)
+set(CAN_BAUDRATE 250)
 
 # Name of target Micro Controller Unit
 # see the available avr-gcc mmcur options for possible values
 set(CMCU "-mmcu=at90can128")
-
-set(CDEFS "${F_CPU} ${CAN_BAUDRATE}")
 
 set(CSTANDARD "-std=c99")
 set(CDEBUG "-g")
@@ -14,8 +12,9 @@ set(CTUNING "-fpack-struct -fshort-enums -funsigned-bitfields -funsigned-char -f
 
 set(COPT "-Os")
 
-set(CFLAGS "${CMCU} ${CDEFS} ${CINCS} ${CSTANDARD} ${CDEBUG} ${CWARN} ${CTUNING} ${COPT} ${CEXTRA}")
-set(CXXFLAGS "${CMCU} ${CDEFS} ${CINCS} ${COPT}")
+add_definitions(-DF_CPU=${F_CPU} -DCAN_BAUDRATE=${CAN_BAUDRATE})
+set(CFLAGS "${CMCU} ${CINCS} ${CSTANDARD} ${CDEBUG} ${CWARN} ${CTUNING} ${COPT} ${CEXTRA}")
+set(CXXFLAGS "${CMCU} ${CINCS} ${COPT}")
 
 set(CMAKE_C_FLAGS ${CFLAGS})
 set(CMAKE_CXX_FLAGS ${CXXFLAGS})
