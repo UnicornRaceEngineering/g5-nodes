eee9e955dcd51768ca1bb8cf24416980a1a0faa1
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2014-09-16 20:15:02 +0200

    Moved all drivers into their own module
    This gives better sepperation of modules so more nodes can share driver
    among each other

--- examples/twi_master/main.c
+++ examples/twi_master/main.c
@@ -26,7 +26,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <avr/interrupt.h>
 
 #include <usart.h>
-#include "m41t81s_rtc.h"
+#include <m41t81s_rtc.h>
 #include <io.h>
 
 
