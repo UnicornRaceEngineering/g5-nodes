0ea21edf95b0f6643e95376df93784167297cc9f
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2014-10-23 21:04:00 +0200

    Added test for the RPM-meter

--- SteeringNode/main.c
+++ SteeringNode/main.c
@@ -50,10 +50,45 @@ FUSES = {.low = 0xFF, .high = 0xD9, .extended = 0xFD};
 #define SHIFT_LIGHT_R		PIN4 // Red rgb light
 #define SHIFT_LIGHT_B		PIN3 // Blue rgb light
 
+#define RPM_PORT		PORTB
+#define RPM_PIN			PIN4
+#define RPM_TCCR		TCCR2A
+#define RPM_OCR			OCR2A
+#define RPM_WGM1		WGM21
+#define RPM_WGM0		WGM20
+#define RPM_CS1			CS21
+#define RPM_CS0			CS20
+#define RPM_COMA1		COM0A1
+#define RPM_COMA0		COM0A0
+
+#define RPM_MAX_VALUE	13000
+#define RPM_MIN_VALUE	3300
+
+
 static void rx_complete(uint8_t mob);
 static void tx_complete(uint8_t mob);
 static void can_default(uint8_t mob);
 
+static int32_t map(int32_t x,
+				   const int32_t from_low, const int32_t from_high,
+				   const int32_t to_low, const int32_t to_high) {
+	if (x < from_low) {
+		x = from_low;
+	} else if (x > from_high) {
+		x = from_high;
+	}
+
+	return (x - from_low) * (to_high - to_low) / (from_high - from_low) + to_low;
+}
+
+static void set_rpm(int16_t rpm) {
+	// Because the PWM signal goes through a low pass filter we loose some
+	// granularity so we must lower the max value or the RPM meter will max out
+	// too soon. The value is determined by increasing the calibration value
+	// until it "looked right".
+	const int8_t calibration = 80;
+	RPM_OCR = map(rpm, RPM_MIN_VALUE, RPM_MAX_VALUE, 0, 0xFF-calibration);
+}
 
 int main(void) {
 	set_canit_callback(CANIT_RX_COMPLETED, rx_complete);
@@ -72,9 +107,15 @@ int main(void) {
 
 	seg7_init();
 
-	// Disable RPM counter for debuggning
-	SET_PIN_MODE(PORTB, PIN4, OUTPUT);
-	IO_SET_LOW(PORTB, PIN4);
+	// init Timer0 PWM PB4 for the RPM-counter
+	{
+		RPM_TCCR |= (1<<RPM_WGM1)|(1<<RPM_WGM0); 	// Fast PWM mode
+		RPM_TCCR |= (1<<RPM_CS1)|(1<<RPM_CS1); 		// F_CPU/64 prescalar
+		RPM_TCCR |= (1<<RPM_COMA1)|(0<<RPM_COMA0);	// Clear RPM_OCR on compare match. Set RPM_OCR at TOP.
+
+		set_rpm(0);
+		SET_PIN_MODE(RPM_PORT, RPM_PIN, OUTPUT);
+	}
 
 	// Shift light RGB LED
 	SET_PIN_MODE(SHIFT_LIGHT_PORT, SHIFT_LIGHT_B, OUTPUT);
@@ -116,7 +157,7 @@ int main(void) {
 				char buff[3+1+1] = {'\0'}; // 3 digits 1 dot and 1 \0
 				snprintf(buff, ARR_LEN(buff), "%d.", i);
 				seg7_disp_str(buff, 0, 2);
-				_delay_us(5000);
+				_delay_us(2500);
 			}
 		}
 
@@ -141,6 +182,7 @@ int main(void) {
 				// In short LOW == ON
 
 				dmux_set_y_low(leds[i]);
+				seg7_disp_char(3, i+'0', false);
 
 				{
 					set_rgb_color(RED); 		_delay_us(2500);
@@ -154,6 +196,30 @@ int main(void) {
 					set_rgb_color(WHITE); 		_delay_us(2500);
 					set_rgb_color(COLOR_OFF); 	_delay_us(2500);
 				}
+			}
+		}
+#endif
+
+#if 1
+		// Test the RPM meter
+		{
+			char buff[14] = {'\0'};
+			// Rev up
+			for (int16_t rpm = 0; rpm <= RPM_MAX_VALUE; rpm += 100) {
+				set_rpm(rpm);
+
+				snprintf(buff, ARR_LEN(buff), "%d.", rpm/100);
+				seg7_disp_str(buff, 4, 6);
+				_delay_us(2500);
+			}
+			_delay_us(5000);
+			// Rev Down
+			for (int16_t rpm = RPM_MAX_VALUE; rpm >= 0; rpm -= 100) {
+				set_rpm(rpm);
+
+				snprintf(buff, ARR_LEN(buff), "%d.", rpm/100);
+				seg7_disp_str(buff, 4, 6);
+				_delay_us(2500);
 			}
 		}
 #endif
